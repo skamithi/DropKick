77722ac2c7d10d1d127bb859ef75206877009bd6
skamithi <skamithi@gmail.com>
2012-11-17 11:31:15 -0500

    Update jquery.dropkick-1.0.0.js
    having js script modify .dk_toggle container messes up the size of my dropdowns. i want full control of width size via CSS.

--- jquery.dropkick-1.0.0.js
+++ jquery.dropkick-1.0.0.js
@@ -112,10 +112,11 @@
       // Build the dropdown HTML
       $dk = _build(dropdownTemplate, data);
 
+      // skamithi: this just messed up my dropdowns.
       // Make the dropdown fixed width if desired
-      $dk.find('.dk_toggle').css({
-        'width' : width + 'px'
-      });
+      //$dk.find('.dk_toggle').css({
+      //  'width' : width + 'px'
+      //});
 
       // Hide the <select> list and place our new one in front of it
       $select.before($dk);
